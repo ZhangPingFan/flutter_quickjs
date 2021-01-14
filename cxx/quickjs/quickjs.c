df6615cfed72af178b0bd952d8b598c5596b91d6
ZhangPingFan <geekheart@foxmail.com>
2021-01-14 22:11:20 +0800

    add support for android
    Signed-off-by: ZhangPingFan <geekheart@foxmail.com>

--- cxx/quickjs/quickjs.c
+++ cxx/quickjs/quickjs.c
@@ -1676,7 +1676,7 @@ static inline size_t js_def_malloc_usable_size(void *ptr)
     return malloc_size(ptr);
 #elif defined(_WIN32)
     return _msize(ptr);
-#elif defined(EMSCRIPTEN)
+#elif defined(EMSCRIPTEN) || __ANDROID_API__ < 17
     return 0;
 #elif defined(__linux__)
     return malloc_usable_size(ptr);
@@ -1750,7 +1750,7 @@ static const JSMallocFunctions def_malloc_funcs = {
     malloc_size,
 #elif defined(_WIN32)
     (size_t (*)(const void *))_msize,
-#elif defined(EMSCRIPTEN)
+#elif defined(EMSCRIPTEN) || __ANDROID_API__ < 17
     NULL,
 #elif defined(__linux__)
     (size_t (*)(const void *))malloc_usable_size,
